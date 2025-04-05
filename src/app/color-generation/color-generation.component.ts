bcb56fa3a35f66d54938058afd2f5ff50b6a8c4c
Parker Jones <pjones11@cheyenne.cs.colostate.edu>
2025-04-05 14:27:01 -0600

    added form

--- src/app/color-generation/color-generation.component.ts
+++ src/app/color-generation/color-generation.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-color-generation',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterOutlet],
   templateUrl: './color-generation.component.html',
   styleUrl: './color-generation.component.css'
 })
 export class ColorGenerationComponent {
+  colorForm: FormGroup = new FormGroup({
+    rows: new FormControl(1, {validators: [Validators.required, Validators.min(1), Validators.max(1000)]}),
+    cols: new FormControl(1, {validators: [Validators.required, Validators.min(1), Validators.max(702)]}),
+    color: new FormControl(1, {validators: [Validators.required, Validators.min(1), Validators.max(10)]})
+  });
 
+  submitForm(event: Event): void{
+    event.preventDefault();
+    console.log(this.colorForm.value.rows);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
-  providers: [appRoutes]
+  providers: [provideRouter(appRoutes)]
 })
   .catch((err) => console.error(err));
