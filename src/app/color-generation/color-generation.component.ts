54f810ad2fc67eb2e0d9a4a7eb5a5e8161deab5d
Parker Jones <pjones11@cheyenne.cs.colostate.edu>
2025-04-05 14:59:10 -0600

    added variables for form vals

--- src/app/color-generation/color-generation.component.ts
+++ src/app/color-generation/color-generation.component.ts
@@ -2,21 +2,30 @@ import { Component} from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+var rows: number = 0;
+var cols: number = 0;
+var color: number = 0;
+
 @Component({
   selector: 'app-color-generation',
   imports: [ReactiveFormsModule, RouterOutlet],
   templateUrl: './color-generation.component.html',
   styleUrl: './color-generation.component.css'
 })
+
+
 export class ColorGenerationComponent {
+  
   colorForm: FormGroup = new FormGroup({
     rows: new FormControl(1, {validators: [Validators.required, Validators.min(1), Validators.max(1000)]}),
     cols: new FormControl(1, {validators: [Validators.required, Validators.min(1), Validators.max(702)]}),
     color: new FormControl(1, {validators: [Validators.required, Validators.min(1), Validators.max(10)]})
   });
 
-  submitForm(event: Event): void{
+  submitForm(event: Event): void {
     event.preventDefault();
-    console.log(this.colorForm.value.rows);
+    rows = this.colorForm.value.rows;
+    cols = this.colorForm.value.cols;
+    color = this.colorForm.value.color;
   }
 }
